d0ccf5abb0ed9606b7d4671ed8ef5011e56231e9
avillaq <villafuertequispealex@gmail.com>
2024-06-09 18:24:50 -0500

    Registramos la nueva aplicacion

--- miProyecto/settings.py
+++ miProyecto/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "tecnologia.apps.TecnologiaConfig",
+    "generadorPDF.apps.GeneradorpdfConfig",
 ]
 
 MIDDLEWARE = [
