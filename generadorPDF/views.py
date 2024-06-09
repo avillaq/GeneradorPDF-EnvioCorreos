6e0626da4e04a351d4ce20d17ffdc6183502a78c
avillaq <villafuertequispealex@gmail.com>
2024-06-09 18:57:04 -0500

    Creamos la vista basada en clases GeneratePDF. Esta vista tiene la funcion get donde enviaremos los datos al template invoice

--- generadorPDF/views.py
+++ generadorPDF/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import View
+from django.template.loader import get_template
 
-# Create your views here.
+from renderers import render_to_pdf
+
+class GeneratePDF(View):
+    def get(self, request, *args, **kwargs):
+        template = get_template('invoice.html')
+        context = {
+            'invoice_id': 123, 
+            'customer_name': 'Jhon Cooper',
+            'amount': 1399.99,
+            'today': "today",
+        }
+        html = template.render(context)
+        return HttpResponse(html)
