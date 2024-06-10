61990ee24136cdef35231caa58001e927040562a
avillaq <villafuertequispealex@gmail.com>
2024-06-09 19:59:29 -0500

    Implementamos la vista usando la funcion send_mail para enviar correos

--- envioEmail/views.py
+++ envioEmail/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 # Create your views here.
 def index(request):
+    send_mail("Hello from PrettyPrinted",
+             "Hello there, This is an automated message.",
+             "sirahib555@morxin.com",
+             ["kaviyof447@cnurbano.com"],
+            fail_silently=False
+    )
     return render(request, 'send/index.html')
